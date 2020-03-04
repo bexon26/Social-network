981f3df7ec2540fb0b464a22b4b00631750b18da
bexon26 <47786103+bexon26@users.noreply.github.com>
2020-03-04 13:02:33 +0300

    Start project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+            <div>
+                <a href='#s'>Profile</a>
+            </div>
+            <div>
+                <a href='#s'>Messages</a>
+            </div>
+            <div>
+                <a href='#s'>News</a>
+            </div>
+            <div>
+                <a href='#s'>Music</a>
+            </div>
+            <div>
+                <a href='#s'>Settings</a>
+            </div>
+        </nav>)
+}
+
+export default Navbar;
